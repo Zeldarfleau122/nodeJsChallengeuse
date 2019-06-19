f289e6796d83452b80c89966ef6c64e421b8bf0d
Zeldarfleau122 <34440348+Zeldarfleau122@users.noreply.github.com>
2019-06-19 14:42:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,31 +4,5 @@
 </head>
 <body>
 <h1>#2 Still possible</h1>
-<?php
-$nice = 1;
-
-if (!isset ($_POST['u'])) $nice = 0;
-if (ereg ('\.', $_POST['u'])) $nice = 0;
-if (ereg ('%', $_POST['u'])) $nice = 0;
-if (ereg ('[0-9]', $_POST['u'])) $nice = 0;
-if (ereg ('http', $_POST['u']) ) $nice = 0;
-if (ereg ('https', $_POST['u']) ) $nice = 0;
-if (ereg ('ftp', $_POST['u'])) $nice = 0;
-if (ereg ('telnet', $_POST['u'])) $nice = 0;
-print($nice) ;
-if ($nice) {
- if (@file_exists ($_POST['u'])) $nice = 0;
- }
-print($nice) ;
-if ($nice) {
- $nice = @file_get_contents ($_POST['u']);
- if ($nice === 'Good Work!') nextpart ();
- }
-print($nice) ;
-?>
-<form method="post">
-u: <input type="text" name="u" /><br />
-<input type="submit" value="Trap!" />
-</form>
 </body>
 </html>
